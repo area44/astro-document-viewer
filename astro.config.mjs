622780cdd0e8188c7ecfe69f623e7561f81c2816
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-16 00:56:25 +0700

    fix astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
@@ -6,6 +7,7 @@ export default defineConfig({
   site: 'https://area44.github.io',
   base: process.env.CI ? '/astro-document-viewer' : undefined,
   integrations: [
+    react(),
     tailwind({
       applyBaseStyles: false,
     }),
